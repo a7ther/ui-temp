b7e1da259c45b52402b1d2bccb86c7950ee1e170
Masataka Tabata <masataka.tabata@uzabase.com>
2022-10-30 11:39:49 +0900

    ハンバーガーメニュー作成

--- app/src/main/java/com/example/ui_temp/view/Menu1Fragment.kt
+++ app/src/main/java/com/example/ui_temp/view/Menu1Fragment.kt
@@ -1,15 +1,14 @@
 package com.example.ui_temp.view
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.ui_temp.R
 
-class MainFragment : Fragment() {
+class Menu1Fragment : Fragment() {
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -17,8 +16,6 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_menu1, container, false)
     }
-
-
 }
